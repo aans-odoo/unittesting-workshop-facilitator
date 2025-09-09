6c792691d4f696d398187c32bc87eb11350c4705
Arib Ansari <aans@odoo.com>
2025-09-09 17:44:58 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Unittest Workshop - Exercise 1A',
+    'summary': 'A simple module having different type of tests cases to showcase types of tests in Odoo',
+    'version': '1.0',
+    'category': 'Workshop',
+    'author': 'Arib Ansari',
+    'assets': {},
+    'installable': True,
+    'application': False,
+    'license': 'AGPL-3'
+}
---
+++ tests/test_something.py
@@ -0,0 +1,6 @@
+from odoo.tests.common import TransactionCase
+
+class TestClass(TransactionCase):
+
+    def test_case(self):
+        self.assertEqual(1, 1)
